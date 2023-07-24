ab21bcfeba0b2a41b7239c68364c414d5375d554
luanfv <luanfrancavieira@gmail.com>
2023-07-24 19:51:33 -0300

    feat: database with knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import fastify from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World!'
+app.get('/transactions', async () => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title: 'transação de teste',
+      amount: 100,
+    })
+    .returning('*')
+
+  return transactions
 })
 
 app
